21287ef2f9ba922ff3bf8b3de0095c53b81007e9
smartysam9821 <smartysam9821@gmail.com>
2021-10-29 13:28:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,7 @@ def main():
         
         # Call the function to extract the data. pass the topic and filename you want the data to be stored in.
         with st.spinner("Please wait, Tweets are being extracted"):
-            get_tweets(Topic , Count=2)
+            get_tweets(Topic , Count=20)
         st.success('Tweets have been Extracted !!!!')    
            
     
